559c40531a3c3f1fca01b698d5b4d592512937da
markv <G00403931@atu.ie>
2022-10-17 21:10:07 +0100

    Prompt user for length

--- src/main/java/ie/atu/week5/ShapeApp.java
+++ src/main/java/ie/atu/week5/ShapeApp.java
@@ -1,4 +1,10 @@
 package ie.atu.week5;
-
+import java.util.Scanner;
 public class ShapeApp {
+    public static void main(String[] args) {
+        System.out.println("Please Enter The Width Of The Rectangle");
+        Scanner input = new Scanner(System.in);
+        double length = input.nextDouble();
+        System.out.println("You Entered : " + length);
+    }
 }
